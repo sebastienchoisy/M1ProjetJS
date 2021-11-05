99b54af6131aebe648760bf46d57343317488759
sebastienchoisy <58088513+sebastienchoisy@users.noreply.github.com>
2021-11-05 18:48:40 +0100

    fix menu layout

--- src/components/restaurantDetails.vue
+++ src/components/restaurantDetails.vue
@@ -3,7 +3,7 @@
   
   <el-col v-if="restaurant" :span="12" :offset="6" class="carré" >
                 <el-row >
-                    <el-col class="détails" v-if="restaurant">
+                    <el-col class="détails">
                         {{restaurant.name}}
                     </el-col>
                 </el-row>
@@ -16,57 +16,25 @@
                 <el-row >
                     <el-col class="col">
                         <div class="gauche1">Adresse</div>
-                        <div class="email" >    <gmap-map v-if="center.lat !== '' && center.lng !== ''"
-                                                          :center="center"
-                                                          :zoom="10"
-                                                          style="width:250px;  height: 300px;"><gmap-marker :position="center" :label="label"></gmap-marker>
-                        </gmap-map> {{restaurant.address.building+' '+restaurant.address.street}} {{restaurant.borough}}</div>
+                        <div class="email" >
+                          <gmap-map v-if="center.lat !== '' && center.lng !== ''" :center="center" :zoom="10" style="width:100%;  height: 300px;">
+                            <gmap-marker :position="center" :label="label"></gmap-marker>
+                        </gmap-map>
+                          <div class="margin-top-5">
+                            {{restaurant.address.building+' '+restaurant.address.street}} {{restaurant.borough}}
+                          </div>
+                        </div>
                     </el-col>
                     <el-col class="col">
                         <div class="droite1">Menu</div>
-                            <el-row style="background-color:white;">
-                              <el-col class="col" :span="8">
-                                <el-row>
-                                  <div class="gauche1">PHOTO</div>
-                                  <div class="prénom" v-for="user in menu" :key="user.id"><el-image :src="user.url"></el-image></div>
-                                </el-row>
-                              </el-col>
-                              <el-col class="col" :span="8">
-                                <div class="droite1">PLAT</div>
-                                <div class="taille"  v-for="user in menu" :key="user.id"> {{user.nom}}  </div>
-                              </el-col>
-                              <el-col class="col" :span="8">
-                                <div class="droite1">PRIX</div>
-                                <div class="taille"  v-for="user in menu" :key="user.id"><a class="prix"> {{user.prix}}€ </a></div>
-                              </el-col>
+                            <el-row class="menu-row" v-for="item in menu" :key="item.id" style="background-color:white;">
+                              <el-col :span="8"><el-image :src="item.url"></el-image></el-col>
+                              <el-col :span="8">{{item.nom}}</el-col>
+                              <el-col :span="8">{{item.prix}}</el-col>
                             </el-row> 
                     </el-col>
                 </el-row>   
             </el-col>
-   <!--<el-main>
-     <el-row>-->
-       <!--<el-col v-if="restaurant" :span="12" :offset="6">
-         <el-row class="blue-container">
-           <el-col :span="12"><el-image :src="photo"></el-image></el-col>
-           <el-col :span="12"> <div class="details-container">
-             <h1> {{this.restaurant.name}}</h1>
-             <span> {{this.restaurant.cuisine}}</span>
-             <span> {{this.restaurant.address.building+' '+this.restaurant.address.street}}</span>
-             <span> {{this.restaurant.borough}}</span>
-             <li v-for="user in menu" :key="user.id">
-               {{user.nom}}
-             </li >
-             <gmap-map v-if="center.lat !== '' && center.lng !== ''"
-                 :center="center"
-                 :zoom="10"
-                 style="width:400px;  height: 400px;"><gmap-marker :position="center" :label="label"></gmap-marker>
-             </gmap-map>
-           </div></el-col>
-         </el-row>
-       </el-col>-->
-    <!-- </el-row>
-   </el-main>-->
-
  </div>
 </template>
 
@@ -135,7 +103,12 @@ h1 {
 .image{
   width:150px;
 }
-
+.menu-row {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding: 1%;
+}
 .taille{
   padding: 40px;
   background-color:white;
